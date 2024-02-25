ee026d24127aa87503c7caa01a8da813a2f68333
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-25 13:33:14 +0000

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,66 @@
-
 import React from "react";
-import './../styles/App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import PrivateRoute from "react-private-route";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+import Login from "./Login";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+    this.isLoggedIn = this.isLoggedIn.bind(this);
+    this.login = this.login.bind(this);
+  }
+  isLoggedIn() {
+    return this.state.isLoggedIn;
+  }
+  login() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  }
+  render() {
+    return (
+      <Router>
+        <div className={"main-container"}>
+          <div>
+            {this.isLoggedIn()
+              ? "Logged in, Now you can enter Playground"
+              : "You are not authenticated, Please login first"}
+          </div>
+          <div>
+            <ul>
+              <li>
+                <Link to="/home">PlayGround</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <Switch>
+            <Route
+              path="/login"
+              component={() => (
+                <Login login={this.login} isLogged={this.state.isLoggedIn} />
+              )}
+            />
+            <PrivateRoute
+              exact
+              path="/home"
+              component={Home}
+              isAuthenticated={!!this.isLoggedIn()}
+              redirect="/login"
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App
